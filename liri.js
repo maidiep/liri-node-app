b6df882c6f34ff91f675e1072cd2b4c5916f34a6
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-05-15 23:51:15 -0400

    code for tweets are working

--- liri.js
+++ liri.js
@@ -10,6 +10,9 @@ var client = new Twitter(keys.twitter);
 var params = {screen_name: 'devShortStack'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    // console.log(tweets);
+    for (var i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].text);
+    }
   }
-});
+}) 
